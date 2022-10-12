ff7db71cc7f208654a090f9979d71708f79639e7
rauland <30706122+rauland@users.noreply.github.com>
2022-10-12 20:49:12 +1100

    refactor: clean up

--- reminder.py
+++ reminder.py
@@ -1,5 +1,8 @@
-import send, report
+import send, report, config
 import pandas as pd
+
+domain = config.get()
+domain = domain['DEFAULT']['domain']
 
 data = report.getjson("open")
 df = pd.json_normalize(data)
@@ -8,7 +11,7 @@ category = df.loc[pd.isnull(df['request.category.name'])]
 assigned = category.loc[category['request.technician.name'] != None]
 techician = assigned['request.technician.name'].unique()
 assigned = assigned[['request.id','request.subject','request.requester.name','request.technician.name']]
-# assigned.loc[assigned['request.id']]=f"""<a href="https://servicedesk.awh.com.au/WorkOrder.do?woMode=viewWO&amp;woID={assigned['request.id']}target="_blank"></a>"""
+# assigned.loc[assigned['request.id']]=f"""<a href=f"{domain}/WorkOrder.do?woMode=viewWO&amp;woID={assigned['request.id']}target="_blank"></a>"""
 
 for tech in techician:
     to_email = assigned.loc[assigned['request.technician.name'] == tech]
@@ -16,4 +19,4 @@ for tech in techician:
     body = to_email.to_html(index=False)
     send.email("⚠️ Unassigned Ticket Categories", body, tech_email, tech)
 
-# <a href="https://servicedesk.awh.com.au/WorkOrder.do?woMode=viewWO&amp;woID=56966" target="_blank"></a>
+# <a href=f"{domain}/WorkOrder.do?woMode=viewWO&amp;woID=56966" target="_blank"></a>
